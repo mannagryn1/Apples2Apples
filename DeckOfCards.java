2e486ab253589a9d8a9b012aa12c4c546f901887
Amanda Lorné <amalor-0@student.ltu.se>
2026-06-30 10:03:39 +0200

    DeckOfCards class added with functionality

---
+++ DeckOfCards.java
@@ -0,0 +1,28 @@
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DeckOfCards {
+    private ArrayList<String> deck;
+
+    public DeckOfCards(String file) throws Exception{
+        this.deck = new ArrayList<>(Files.readAllLines(Paths.get("./", file), StandardCharsets.ISO_8859_1));
+    }
+
+    public void shuffle() {
+        Random rnd = ThreadLocalRandom.current();
+        for(int i=deck.size()-1; i>0; i--) {
+			int index = rnd.nextInt(i+1);
+			String a = deck.get(index); deck.set(index, deck.get(i)); deck.set(i, a); // SWAP
+		}
+    }
+
+    public String removeTopCard() {
+        String card = this.deck.get(0);
+        this.deck.remove(0);
+        return card;
+    }
+}
