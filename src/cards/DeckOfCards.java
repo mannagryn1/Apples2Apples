28bb5ba096749b56b0a26ca34f8957fb9976ddd8
mannagryn1 <amalor-0@student.ltu.se>
2026-08-18 10:53:16 +0200

    compiled and running

--- src/cards/DeckOfCards.java
+++ src/cards/DeckOfCards.java
@@ -11,7 +11,7 @@ public class DeckOfCards {
 
     public DeckOfCards(String file) {
         try{    
-            this.deck = new ArrayList<>(Files.readAllLines(Paths.get("Apples2Apples/src/cards/cardDecks/", file), StandardCharsets.ISO_8859_1));}
+            this.deck = new ArrayList<>(Files.readAllLines(Paths.get(".java/src/cards/cardDecks/", file), StandardCharsets.ISO_8859_1));}
         catch(Exception e) {
             System.out.println("Something went wrong when creating a deck of cards with file " +e);
         }
