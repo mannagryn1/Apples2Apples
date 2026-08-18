8f6aa321353dc6f25bda0e0673a8036cba26cce8
mannagryn1 <amalor-0@student.ltu.se>
2026-08-18 11:06:44 +0200

    final fixes??????

--- src/cards/DeckOfCards.java
+++ src/cards/DeckOfCards.java
@@ -1,6 +1,7 @@
 package src.cards;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Random;
@@ -10,8 +11,17 @@ public class DeckOfCards {
     private ArrayList<String> deck;
 
     public DeckOfCards(String file) {
-        try{    
-            this.deck = new ArrayList<>(Files.readAllLines(Paths.get(".java/src/cards/cardDecks/", file), StandardCharsets.ISO_8859_1));}
+        try{                //works for tests
+            this.deck = new ArrayList<>(Files.readAllLines(Paths.get("./Apples2Apples/cardDecks/", file), StandardCharsets.ISO_8859_1));
+        }
+        catch(NoSuchFileException e){
+            try{            //works for running the program
+                this.deck = new ArrayList<>(Files.readAllLines(Paths.get("./cardDecks/", file), StandardCharsets.ISO_8859_1));
+            }
+            catch(Exception f) {
+            System.out.println("Something went wrong when creating a deck of cards with file " +f);
+        }
+        }
         catch(Exception e) {
             System.out.println("Something went wrong when creating a deck of cards with file " +e);
         }
